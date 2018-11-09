6fee0d9330c07ddf2487ec4fc12741152cfbba44
Songzhichun <306677018@qq.com>
2018-11-09 18:15:27 +0800

    init family project

---
+++ src/main/java/com/never/yc/YcApplication.java
@@ -0,0 +1,24 @@
+package com.never.yc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+@RequestMapping("/api/home")
+public class YcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YcApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(String name) {
+        return "Hello ：" + name + " welcome to family ";
+    }
+
+}
